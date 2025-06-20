663cdafcc6816a11bcb37f7a2de955a8e5707418
BonnkeWebs <80023449+bonnke2022@users.noreply.github.com>
2025-06-21 00:26:21 +0100

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,23 @@
+"use client";
+import Link from "next/link";
 import LinksDropdown from "./LinksDropdown";
+import ThemeToggle from "./ThemeToggle";
 
 const Nav = () => {
   return (
-    <nav className="bg-muted py-4 px-16 lg:hidden px-4 flex items-center justify-between">
+    <nav className="bg-muted py-4 md:px-16 px-4 flex items-center justify-between">
       <div>
         <LinksDropdown />
       </div>
-      {/* <div className="flex items-center gap-x-4">
+      <div className="flex items-center gap-x-4">
         <ThemeToggle />
-      </div> */}
+        <Link
+          href="/shop"
+          className="border-2 rounded-lg px-4 py-2 hover:bg-blue-200"
+        >
+          Back to Shop
+        </Link>
+      </div>
     </nav>
   );
 };
