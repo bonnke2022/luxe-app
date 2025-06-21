fd76742e151b68cce329bede2750a9e78df99f2e
BonnkeWebs <80023449+bonnke2022@users.noreply.github.com>
2025-06-21 01:21:22 +0100

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ThemeProvider } from "@/components/ThemeProvider";
+
 import React, { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -19,18 +19,11 @@ function Providers({ children }: { children: ReactNode }) {
       })
   );
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>{children}</Provider>
         <Toaster />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
-    </ThemeProvider>
   );
 }
 
